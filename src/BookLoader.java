80f1ddf834638cb40fa1f219df3973b2f072c975
thor <thor@cphbusiness.dk>
2021-10-07 10:49:43 +0200

    static bookloader fix

--- src/BookLoader.java
+++ src/BookLoader.java
@@ -5,14 +5,12 @@ import java.util.Random;
 public class BookLoader {
     static Random rd = new Random();
 
-
     public static void loadBooks(Book[] books) {
         for(int i=0;i<books.length;i++) {
-            StringBuilder sb = new StringBuilder();
             int rd2 = rd.nextInt(50)+40;
             int rd = (int)(Math.random()*1000);
+            // TODO:Hvad sker der mon her?
             String s= String.format("%c%c",rd2,rd2+1);
-
             Book b = new Book(i,"Kurts "+s,rd2);
             books[i]=b;
         }
